03537bd610b3923a1e914356f780fce9e643efef
jrakam <jrakam@albany.edu>
2022-11-03 15:01:32 -0400

    sprint-1 Welcomepage frondend

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,26 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserDetailsService } from '../user-details.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AlertService } from "../../services/alert/alert.service";
+import { AuthService } from "../../services/auth/auth.service";
+import { UserDetailsService } from "../../services/user-details.service";
 
 @Component({
-  selector: 'app-welcome-page',
-  templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
+  selector: "app-welcome-page",
+  templateUrl: "./welcome-page.component.html",
+  styleUrls: ["./welcome-page.component.css"],
 })
 export class WelcomePageComponent implements OnInit {
+  isUserLoggedIn: boolean = false;
+  lastName: string = "";
+  showAlert = false;
 
-  isSearch : boolean = true;
-  public serv : any;
-
-  constructor(private router : Router,private service: UserDetailsService) { 
-    this.serv = service;
-  }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit(): void {
-console.log(this.serv.fromsignup);
-  }
-  clickLogin(value : boolean){
-    this.isSearch = value;
+    this.authService.userLoggedinStatus();
 
+    this.authService.getUserLoginStatus$.subscribe((loginStatus) => {
+      this.isUserLoggedIn = loginStatus;
+      if (this.isUserLoggedIn) {
+        const user = localStorage.getItem("user");
+        const userDetail = user ? JSON.parse(user) : {};
+        this.lastName = userDetail?.lastName;
+      } else {
+        this.lastName = "User";
+      }
+    });
+
+    this.alertService.getStatus$.subscribe((status) => {
+      this.showAlert = true;
+    });
+  }
+
+  signOut() {
+    this.authService.logout();
+    this.router.navigate(["/home"]);
+  }
+
+  showLink(item: string) {
+    return !(this.router.url === item);
   }
 }
