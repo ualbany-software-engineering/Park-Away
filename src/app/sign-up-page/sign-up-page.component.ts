48c9f604b2dfca3b33e4e09bd043e21630db8e83
Thrishitha Dendi <tdendi@albany.edu>
2022-11-03 15:06:06 -0400

    Spint 1 signup page frontend

--- src/app/sign-up-page/sign-up-page.component.ts
+++ src/app/sign-up-page/sign-up-page.component.ts
@@ -1,66 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { Router } from '@angular/router';
-import { UserDetailsService } from '../user-details.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AlertService } from "../../services/alert/alert.service";
+import { UserDetailsService } from "../../services/user-details.service";
 
 @Component({
-  selector: 'app-sign-up-page',
-  templateUrl: './sign-up-page.component.html',
-  styleUrls: ['./sign-up-page.component.css']
+  selector: "app-sign-up-page",
+  templateUrl: "./sign-up-page.component.html",
+  styleUrls: ["./sign-up-page.component.css"],
 })
 export class SignUpPageComponent implements OnInit {
-
-  //constructor() { }
-  type : string ="password";
-  isText : boolean = false;
-  eyeIcon :string = "fa-eye-slash";
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
   signUpForm!: FormGroup;
-  constructor(private fb: FormBuilder,private service : UserDetailsService,
-    private router : Router){}
+  passwordMatch = false;
+  isFormSubmitted = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private userDetailsService: UserDetailsService,
+    private router: Router,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit(): void {
-    this.signUpForm= this.fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      phone: ['', Validators.required],
-      })
-  };
-  public hideShowPass(){
-    this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-sye-slash";
-    this.isText ? this.type = "text" : this.type = "password";
+    this.signUpForm = this.fb.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}"),
+        ],
+      ],
+      password: ["", Validators.required],
+      confirmPassword: ["", Validators.required],
+      phone: ["", Validators.required],
+    });
   }
-  
-  onSignup(){
-    if(this.signUpForm.valid){
-      //perform logic for signup
-      var obj ={
-    
-      }
-      console.log(this.signUpForm.value)
-    }
-    else{
-      //logic for throwing error
-    }
-    }
-    Register(formGroup : FormGroup){
-      debugger
-      var obj ={
-        "firstName" : formGroup.get('firstName')?.value,
-        "lastName" : formGroup.get('lastName')?.value,
-        "email" : formGroup.get('email')?.value,
-        "password" : formGroup.get('password')?.value,
-        "phone" : formGroup.get('phone')?.value
-       }
-       this.service.fromsignup = true;
-       this.service.post(obj).subscribe(o =>{
-        console.log("success");
-        this.service.firstname = formGroup.get('firstName')?.value;
-        this.router.navigate(['/search']);
-        
-       })
-    }
- }
 
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? (this.eyeIcon = "fa-eye") : (this.eyeIcon = "fa-sye-slash");
+    this.isText ? (this.type = "text") : (this.type = "password");
+  }
+
+  register() {
+    this.isFormSubmitted = true;
+    if (!this.signUpForm.valid) {
+      this.signUpForm.markAllAsTouched();
+      return;
+    }
+
+    if (
+      this.signUpForm.value.password != this.signUpForm.value.confirmPassword
+    ) {
+      this.passwordMatch = false;
+      return;
+    } else {
+      this.passwordMatch = true;
+    }
+
+    this.userDetailsService.register(this.signUpForm.value).subscribe(
+      (data) => {
+        console.log(JSON.stringify(data));
+        this.router.navigate(["/login"]);
+      },
+      (err) => {
+        this.alertService.updateStatus({
+          success: false,
+          error: true,
+          message: "Unable to register, please try after sometime",
+        });
+      }
+    );
+  }
+
+  get passwordMatchError() {
+    return (
+      this.signUpForm.getError("mismatch") &&
+      this.signUpForm.get("confirmPassword")?.touched
+    );
+  }
+}
